5aa9ff2d830af0ba86885c19a2bef0a1b8ec58e0
Hadia <167001489+HadiaPlucky@users.noreply.github.com>
2025-04-16 10:29:31 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+// Optional JavaScript for interactivity (if needed)
+document.querySelector('.cta-btn').addEventListener('click', function () {
+    alert('Thank you for reaching out! I look forward to working with you!');
+});
